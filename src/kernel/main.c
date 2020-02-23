3ec8401295cd2c48c5ffe2f787d23f4acd7db35f
sunyiming <18811311416@sina.cn>
2020-02-23 16:04:00 +0800

    memory get kernel pages finish

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,6 +3,10 @@
 void main(void){
 	put_str("I am kernel\n");
 	init_all();
-	asm volatile("sti");
+	void* addr = get_kernel_pages(3);
+	put_str("\n get_kernel_pages start vaddr is ");
+	put_int((uint32_t)addr);
+	put_str("\n");
+	//asm volatile("sti");
 	while(1);
 }
--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -1,10 +1,17 @@
 #include "memory.h"
+#include "bitmap.h"
 #include "stdint.h"
+#include "global.h"
 #include "print.h"
+#include "string.h"
+#include "interrupt.h"
 
 #define PG_SIZE 4096
 #define MEM_BITMAP_BASE 0xc009a000
 #define K_HEAP_START 0xc0100000
+
+#define PDE_IDX(addr) ((addr & 0xffc00000) >> 22) // 虚拟地址高10位，pde
+#define PTE_IDX(addr) ((addr & 0x003ff000) >> 12) // 虚拟地址中间10位，pte
 
 struct pool {
 	struct bitmap pool_bitmap;
@@ -15,6 +22,109 @@ struct pool {
 struct pool kernel_pool, user_pool;
 struct virtual_addr kernel_vaddr;
 
+// 在pf表示的虚拟内存池中申请pg_cnt个虚拟页，成功返回起始地址，失败返回NULL
+static void* vaddr_get(enum pool_flags pf, uint32_t pg_cnt) {
+	int vaddr_start = 0, bit_idx_start = -1;
+	uint32_t cnt = 0;
+	if (pf == PF_KERNEL) {
+		bit_idx_start = bitmap_scan(&kernel_vaddr.vaddr_bitmap, pg_cnt);
+		if (bit_idx_start == -1) {
+			return NULL;
+		}
+		while(cnt < pg_cnt) {
+			bitmap_set(&kernel_vaddr.vaddr_bitmap, bit_idx_start + cnt++, 1);
+		}
+		vaddr_start = kernel_vaddr.vaddr_start + bit_idx_start * PG_SIZE;
+	} else {
+		// 用户内存池，将来再说
+	}
+	return (void*)vaddr_start;
+}
+
+// 得到虚拟地址vaddr对应的pte指针
+uint32_t* pte_ptr(uint32_t vaddr) {
+	uint32_t* pte = (uint32_t*)(0xffc00000 + ((vaddr & 0xffc00000) >> 10) + PTE_IDX(vaddr) * 4);
+	return pte;
+}
+
+// 得到虚拟地址vaddr对应的pde指针
+uint32_t* pde_ptr(uint32_t vaddr) {
+	uint32_t* pde = (uint32_t*)((0xfffff000) + PDE_IDX(vaddr) * 4);
+	return pde;
+}
+
+//在m_pool指向的物理内存池中分配1个物理页
+static void* palloc(struct pool* m_pool) {
+	//找到一个物理页
+	int bit_idx = bitmap_scan(&m_pool->pool_bitmap, 1);
+	if (bit_idx == -1) {
+		return NULL;
+	}
+	// 将此位置1将
+	bitmap_set(&m_pool->pool_bitmap, bit_idx, 1);
+	uint32_t page_phyaddr = ((bit_idx * PG_SIZE) + m_pool->phy_addr_start);
+	return (void*)page_phyaddr;
+}
+
+// 页表中添加虚拟地址_vaddr与物理地址_page_phyaddr的映射
+static void page_table_add(void* _vaddr, void* _page_phyaddr) {
+	uint32_t vaddr = (uint32_t)_vaddr;
+	uint32_t page_phyaddr = (uint32_t)_page_phyaddr;
+	uint32_t* pde = pde_ptr(vaddr);
+	uint32_t* pte = pte_ptr(vaddr);
+	
+	// 判断页目录项的p位，为1表示该表已存在
+	if (*pde & 0x00000001) {
+		if(!(*pte & 0x00000001)) {
+			*pte = (page_phyaddr | PG_US_U | PG_RW_W | PG_P_1);
+		} else {
+			// pte repeat
+		}
+	} else {
+		// 页目录项不存在，先创建页目录项，再创建页表项
+		uint32_t pde_phyaddr = (uint32_t)palloc(&kernel_pool);
+		*pde = (pde_phyaddr | PG_US_U | PG_RW_W | PG_P_1);
+		memset((void*)((int)pte & 0xfffff000), 0, PG_SIZE);
+		*pte = (page_phyaddr | PG_US_U | PG_RW_W | PG_P_1);
+	}
+}
+
+// 分配pg_cnt个页空间，成功返回起始虚拟地址，失败返回NULL
+void* malloc_page(enum pool_flags pf, uint32_t pg_cnt) {
+	// 1 通过 vaddr_get 在虚拟内存池中申请虚拟地址
+	// 2 通过 palloc 在物理内存池中申请物理页
+	// 3 通过 page_table_add 将以上得到的虚拟地址和物理地址在页表中完成映射
+	void* vaddr_start = vaddr_get(pf, pg_cnt);
+	if (vaddr_start == NULL) {
+		return NULL;
+	}
+	
+	uint32_t vaddr = (uint32_t)vaddr_start;
+	uint32_t cnt = pg_cnt;
+	struct pool* mem_pool = pf & PF_KERNEL ? &kernel_pool : &user_pool;
+	
+	// 虚拟地址和物理地址逐个映射
+	while (cnt-- > 0) {
+		void* page_phyaddr = palloc(mem_pool);
+		if (page_phyaddr == NULL) {
+			return NULL;
+		}
+		page_table_add((void*)vaddr, page_phyaddr);
+		vaddr += PG_SIZE;
+	}
+	return vaddr_start;
+}
+
+// 从内核物理内存池中申请1页内存，成功返回虚拟地址，失败NULL
+void* get_kernel_pages(uint32_t pg_cnt) {
+	void* vaddr = malloc_page(PF_KERNEL, pg_cnt);
+	if (vaddr != NULL) {
+		memset(vaddr, 0, pg_cnt * PG_SIZE);
+	}
+	return vaddr;
+}		
+
+// 初始化内存池
 static void mem_pool_init(uint32_t all_mem) {
 	put_str("  mem_pool_init start\n");
 	uint32_t page_table_size = PG_SIZE * 256;
@@ -65,6 +175,7 @@ static void mem_pool_init(uint32_t all_mem) {
 	put_str("   mem_pool_init done\n");
 }
 
+// 初始化内存
 void mem_init() {
 	put_str("mem_init start\n");
 	//uint32_t mem_bytes_total = (*(uint32_t*)(0xb00));
